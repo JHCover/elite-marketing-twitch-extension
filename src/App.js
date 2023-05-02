f8de1af7ec303a81dc26f36bd455217be3770264
Jared Cover <jhcover@gmail.com>
2023-05-02 16:36:25 -0400

    change socket library

--- src/App.js
+++ src/App.js
@@ -5,71 +5,30 @@ import {Dots} from "react-activity";
 import "react-activity/dist/library.css";
 import TitleImage from "./title.png"
 import SubTitleImage from "./subTitle.png"
-import {SocketProvider} from './Context/SocketContext';
+import { io } from "socket.io-client";
 
+const socket = io("https://elitemarketingstats.com");
 
 function App() {
 
-    const [yesCount, setYesCount] = useState(0);
-    const [noCount, setNoCount] = useState(0);
-    const [spinnerOn, setSpinnerOn] = useState(false);
-
-    const socket = useRef(null);
+    const [pitchStats, setPitchStats] = useState({})
+    const [spinnerOn, setSpinnerOn] = useState()
 
 
-    // useEffect(() => {
-    //     socket.current = new WebSocket('http://localhost:5000')
-    //
-    //     socket.current.addEventListener('open', e => {
-    //         console.log('WebSocket is connected')
-    //         getInitialValues()
-    //         setSpinnerOn(false)
-    //     })
-    //     socket.current.addEventListener('close', e => console.log('WebSocket is closed'))
-    //
-    //     socket.current.addEventListener('error', e => console.error('WebSocket is in error', e))
-    //
-    //     socket.current.addEventListener('message', e => {
-    //         console.log(`number of yes's: ${JSON.parse(e.data).message}`)
-    //         let message = JSON.parse(e.data).message
-    //         console.log(`number of yes's:`, message.yesCount)
-    //         setYesCount(message.yesCount)
-    //         setNoCount(message.noCount)
-    //         if (spinnerOn) setSpinnerOn(false)
-    //     })
-    //
-    //     return () => {
-    //         socket.current.close();
-    //         console.log("websocket closed")
-    //     };
-    // }, []);
-
-    // useEffect(() => {
-    //     setInterval(sendHeartbeat, 300000)
-    // }, [])
-
-    // const sendHeartbeat = () => {
-    //     const payload = {
-    //         action: 'message',
-    //         type: 'heartbeat'
-    //     }
-    //     console.log("heartbeat sent")
-    //     socket.current.send(JSON.stringify(payload))
-    //
-    // }
-
-    const getInitialValues = async () => {
-        const payload = {
-            action: 'message',
-            type: 'getInitialValues'
-        }
-        await socket.current.send(JSON.stringify(payload))
-        return Promise.resolve()
-    }
+    useEffect(() => {
+        // receive initial data load
+        socket.on("initial", (stats) => {
+            console.log("initialStats", stats)
+            setPitchStats(stats[0])
+        });
+        // receive a broadcast update
+        socket.on("statsUpdate", (update) => {
+            setPitchStats(update)
+        })
+    }, [])
 
 
     return (
-        <SocketProvider>
             <div className="App">
                 {spinnerOn ? <Dots color="#0000FF" size={32} speed={1} animating={true}/> :
                     <div className="Border">
@@ -79,15 +38,14 @@ function App() {
                         </div>
                         <header className="Header">Lifetime Totals</header>
                         <p className="YesText">
-                            YES: {yesCount}
+                            YES: {pitchStats.yesCount}
                         </p>
                         <p className="NoText">
-                            NO: {noCount}
+                            NO: {pitchStats.noCount}
                         </p>
                     </div>
                 }
             </div>
-        </SocketProvider>
     );
 }
 
